e6d19ebb6836d1b56f779a53014c4092a8dc0185
Kyle <kyle.hongkai@qq.com>
2026-06-22 16:03:55 +0800

    去除多余代码

--- app/main.py
+++ app/main.py
@@ -14,10 +14,6 @@ from app.core.config import get_runtime_config
 from app.core.exceptions import AppException
 from app.core.logging import RequestLoggingMiddleware, setup_logging
 from app.core.response import error_response
-def cde():
-    pass
-def abc():
-    pass
 
 def create_app() -> FastAPI:
     runtime_config = get_runtime_config()
